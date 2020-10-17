9f2132b13149f428a489b4fc94cd8bdae426cd38
Pedro Gonzales <pedro281195@gmail.com>
2020-10-17 11:00:33 -0500

    Correcion Ejercicio2

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -1,35 +1,21 @@
 /*Ejercicio #2: Escriba una función recursiva de JavaScript que determine
 si un número es par o impar*/
-
-//Opción 1
-function isParImpar1(number){
-    number = Math.abs(number);
-    if(number === 0)
-        return "Par";
-    else if(number === 1)
-       return "Impar";
+let index = 0;
+function parImpar(arrayNumber){
+    let array = [];
+    array = arrayNumber;
+    let arrayPar = [];
+    let arrayImpar = [];
+    
+    if(array[index]%2 === 0)
+        console.log(`Par: ${array[index]}`);
     else
-      return isParImpar1(number-2);
+        console.log(`Impar: ${array[index]}`);
+    index++;
+    if(index == arrayNumber.length) 
+        return;
+    else
+        parImpar(arrayNumber)
 }
+parImpar([21,2,53,1,50]);
 
-console.log(isParImpar1(52));
-console.log(isParImpar1(51));
-
-//Opción 2
-function isParImpar2(number, opc='P'){
-    number = Math.abs(number);
-    if(opc === 'P'){
-        if(number === 0)
-            return 'Par';
-        else 
-            return isParImpar2(number-1,'I');
-    }else if(opc === 'I'){
-        if(number === 0)
-            return 'Impar';
-        else
-            return isParImpar2(number-1, 'P');
-    }
-}
-
-console.log(isParImpar2(53));
-console.log(isParImpar2(54));
